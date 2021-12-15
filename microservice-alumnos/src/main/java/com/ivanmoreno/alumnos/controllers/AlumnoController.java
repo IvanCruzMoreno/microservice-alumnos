57c76224138c6a155958e14b7f1a1797cc9a2361
ivan <ivanmoreno405@gmail.com>
2021-12-15 09:54:03 -0600

    create, edit and download - photo field

--- microservice-alumnos/src/main/java/com/ivanmoreno/alumnos/controllers/AlumnoController.java
+++ microservice-alumnos/src/main/java/com/ivanmoreno/alumnos/controllers/AlumnoController.java
@@ -1,18 +1,25 @@
 package com.ivanmoreno.alumnos.controllers;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ivanmoreno.alumnos.services.AlumnoService;
 import com.ivanmoreno.commons.controllers.CommonController;
@@ -47,6 +54,54 @@ public class AlumnoController extends CommonController<Alumno, AlumnoService>{
 	public ResponseEntity<?> filter(@PathVariable String value) {
 		List<Alumno> alumnos = this.service.findByNombreOrApellido(value);
 		return ResponseEntity.ok(alumnos);
+	}
+
+	@PostMapping("/create-with-photo")
+	public ResponseEntity<?> createWithPhoto(@Valid Alumno alumno, BindingResult result, @RequestParam MultipartFile archivo) throws IOException {
+		if(!archivo.isEmpty()) {
+			alumno.setPhoto(archivo.getBytes());
+		}
+		return super.create(alumno, result);
+	}
+	
+	@PutMapping("/edit-with-photo/{id}")
+	public ResponseEntity<?> editWithPhoto(@Valid Alumno alumno, BindingResult result, @PathVariable Long id, @RequestParam MultipartFile archivo) throws IOException {
+		
+		if(result.hasErrors()) {
+			return this.validar(result);
+		}
+		
+		Optional<Alumno> alumnoOpt = this.service.findById(id);
+		
+		if(!alumnoOpt.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		Alumno alumnoDB = alumnoOpt.get();
+		alumnoDB.setNombre(alumno.getNombre());
+		alumnoDB.setApellido(alumno.getApellido());
+		alumnoDB.setEmail(alumno.getEmail());
+		
+		if(!archivo.isEmpty()) {
+			alumnoDB.setPhoto(archivo.getBytes());
+		}
+		
+		Alumno alumnoSaved = this.service.save(alumnoDB);
+		return ResponseEntity.status(HttpStatus.CREATED).body(alumnoSaved);
+	}
+	
+	@GetMapping("/download/img/{id}")
+	public ResponseEntity<?> showImage(@PathVariable Long id) {
+		
+		Optional<Alumno> entity = service.findById(id);
+		
+		if(!entity.isPresent() || entity.get().getPhoto() == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		Resource image = new ByteArrayResource(entity.get().getPhoto());
+		
+		return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(image);
 	}
 	
 }
